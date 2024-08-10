5a9ee0df86b3bb7e79b3c776ce3e1339e00e3b5a
Alex Kalinichev <akalinichev@natera.com>
2024-08-10 13:07:47 -0700

    ad-hoc polymorphism

--- build.sbt
+++ build.sbt
@@ -12,6 +12,15 @@ lazy val practice = project
     )
   )
 
+lazy val `design-patterns` = project
+  .settings(
+    libraryDependencies ++= Seq(
+      "org.testng" % "testng" % "7.7.1",
+      "org.scalatest" %% "scalatest" % "3.2.3",
+      "org.slf4j" % "slf4j-simple" % "1.7.36"
+    )
+  )
+
 lazy val `warski-di` = project
   .settings(
     libraryDependencies ++= Seq(
---
+++ design-patterns/src/main/scala/AdHocPolymorphism.scala
@@ -0,0 +1,34 @@
+object AdHocPolymorphism {
+
+  trait Adder[T] {
+    def sum(a: T, b: T): T
+  }
+
+  object Adder {
+    def sum[T: Adder](a: T, b: T): T = implicitly[Adder[T]].sum(a, b)
+
+    // implicit conversion from T to Adder[T]:
+
+    //    implicit val int2Adder: Adder[Int] = new Adder[Int] {
+    //      override def sum(a: Int, b: Int): Int = a + b
+    //    }
+
+    implicit val int2Adder: Adder[Int] = (a: Int, b: Int) => a + b
+    /**
+     * This is equivalent to:
+     * {{{
+     * implicit val int2Adder: Adder[Int] = new Adder[Int] {
+     *   override def sum(a: Int, b: Int): Int = a + b
+     * }
+     * }}}
+     */
+    implicit val string2Adder: Adder[String] = (a: String, b: String) =>
+      s"$a concatenated with $b"
+  }
+
+  def main(args: Array[String]): Unit = {
+    import Adder._
+    println(sum(1, 2))
+    println(sum("Hello", "World"))
+  }
+}
